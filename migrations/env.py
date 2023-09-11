cf8e351b8488c0e4678807afd04953d1cdb350e0
Sergey <ssnovak@stud.kpfu.ru>
2023-09-11 15:31:25 +0300

    + new models

--- migrations/env.py
+++ migrations/env.py
@@ -6,7 +6,8 @@ from sqlalchemy import pool
 from alembic import context
 
 from src.config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
-from src.user.models import metadata
+from src.user.models import Role, User
+from src.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -29,7 +30,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- src/user/models.py
+++ src/user/models.py
@@ -1,31 +1,33 @@
 from datetime import datetime
 
-from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON, Boolean, DATE
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, JSON, TIMESTAMP
+from sqlalchemy.orm import relationship
 
-metadata = MetaData()
-
-roles = Table(
-    "roles",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("name", String, nullable=False),
-    Column("permissions", JSON),
-)
+from src.database import Base
 
 
-users = Table(
-    "users",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("email", String, nullable=False),
-    Column("username", String, nullable=False),
-    Column("password", String, nullable=False),
-    Column("first_name", String, nullable=False),
-    Column("last_name", String),
-    Column("date_of_birth", TIMESTAMP, nullable=False),
-    Column("is_active", Boolean, default=True),
-    Column("last_login_at", TIMESTAMP),
-    Column("phone_number", String),
-    Column("registered_at", TIMESTAMP, default=datetime.utcnow),
-    Column("role_id", Integer, ForeignKey("roles.id")),
-)
+class Role(Base):
+    __tablename__ = "roles"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    permissions = Column(JSON)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String)
+    date_of_birth = Column(TIMESTAMP, nullable=False)
+    is_active = Column(Boolean, default=True)
+    last_login_at = Column(TIMESTAMP)
+    phone_number = Column(String)
+    registered_at = Column(TIMESTAMP, default=datetime.utcnow)
+    role_id = Column(Integer, ForeignKey("roles.id"))
+
+    role = relationship("Role")
